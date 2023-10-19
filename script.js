0304cda8af399ccd9e23d0e7080bc69c4d68b534
Alejandro <108769198+DeveloperAlejandroS@users.noreply.github.com>
2023-10-19 08:17:12 -0500

    Update script.js
    Added color player based card`s color change

--- script.js
+++ script.js
@@ -13,3 +13,24 @@ reproductor.addEventListener('pause', () => {
     OnAir.style.display = 'none';
     Offline.style.display = 'block';
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Obtén el reproductor de audio
+    var audioPlayer = document.getElementById('reproductor');
+    
+    // Función para detectar si el reproductor de audio es oscuro
+    function esReproductorOscuro() {
+        return window.matchMedia('(prefers-color-scheme: dark)').matches;
+    }
+
+    // Obtén la tarjeta
+    var tarjeta = document.querySelector('.card');
+
+    // Establece un color predeterminado para la tarjeta
+    tarjeta.style.backgroundColor = '#f1f3f4'; // Fondo blanco por defecto
+
+    // Si el reproductor de audio es oscuro, cambia el color de fondo de la tarjeta
+    if (esReproductorOscuro()) {
+        tarjeta.style.backgroundColor = '#1a1a1a'; // Fondo oscuro si el modo oscuro está activado
+    }
+});
